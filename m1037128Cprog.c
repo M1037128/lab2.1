e833e5f27444f5ca4814ad1bc22862e880d726d4
Ubuntu <balaji.baskar2@mindtree.com>
2020-05-21 14:54:18 +0000

    This is my Devops practice lab2.1 file commit

---
+++ m1037128Cprog.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(int argc, char args[]) {
+	printf("This is my DevOps project lab 2.1 C prog file. \n");
+}
+
---
+++ m1037128Javaprog.java
@@ -0,0 +1,6 @@
+class m1037128MyClass {
+	
+	public static void main (String[] args) {
+		System.out.println(" This is my DevOps lab Java program \n");
+	}
+}
